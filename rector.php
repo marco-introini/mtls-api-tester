5a2f09ad239d1fd67bbafd6edc98aaa25dfcf03b
Marco "Mint" Introini <mint.dev@pm.me>
2024-12-23 17:02:17 +0100

    preparing for laravel 11 update

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Set\LaravelLevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/lang',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withRules([
+        AddGenericReturnTypeToRelationsRector::class,
+    ])
+    ->withSets([
+        LaravelLevelSetList::UP_TO_LARAVEL_110,
+    ]);
