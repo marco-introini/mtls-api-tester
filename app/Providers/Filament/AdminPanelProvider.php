96232875a68c07e0de328be998e080bffd741bbb
Marco "Mint" Introini <mint.dev@pm.me>
2024-12-24 09:38:52 +0100

    Set max content width to 'full' in AdminPanelProvider.
    This update ensures the admin panel's content spans the full width, enhancing the layout's appearance and usability. The change aligns with the existing configuration for customization and style consistency.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -31,6 +31,7 @@ class AdminPanelProvider extends PanelProvider
             ->passwordReset()
             ->registration()
             ->profile()
+            ->maxContentWidth('full')
             ->colors([
                 'primary' => Color::Blue,
             ])
