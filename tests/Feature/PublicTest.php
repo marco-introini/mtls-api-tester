b8429ebd8fda4674fe8b8b3562b0ead6876fc706
Marco "Mint" Introini <mint.dev@pm.me>
2024-12-24 08:29:44 +0100

    Remove redundant home route definition
    The route pointing to the home view was removed as it is no longer required. This simplifies the web routes file and eliminates unnecessary code.

--- tests/Feature/PublicTest.php
+++ tests/Feature/PublicTest.php
@@ -8,6 +8,3 @@ it('has public page')
     ->get('/')
     ->assertOk();
 
-it('has GitHub link')
-    ->get('/')
-    ->assertSee('Github');
--- tests/Feature/UrlTest.php
+++ tests/Feature/UrlTest.php
@@ -3,26 +3,4 @@
 use App\Models\Url;
 use App\Services\UrlTester;
 
-beforeEach(function (): void {
-    Url::create([
-        'id' => 1,
-        'name' => 'Example Url',
-        'certificate_id' => null,
-        'url' => 'http://www.thomas-bayer.com/axis2/services/BLZService',
-        'request' => 'WRONG SOAP',
-    ]);
-});
 
-test('Database is Created', function (): void {
-    expect(Url::whereId(1))->not->toBeNull();
-});
-
-test('Example Url is Created', function (): void {
-    expect(Url::whereId(1)->value('name'))->toEqual('Example Url');
-});
-
-test('Example Url Called and AxisFault returned', function (): void {
-    $tester = new UrlTester(Url::whereId(1)->first());
-    $response = $tester->executeTest();
-    expect($response)->toContain('org.apache.axis2.AxisFault');
-});
